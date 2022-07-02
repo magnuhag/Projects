4851d0def56a9fe5bfe068edf7a333d27d30e4fa
magnuhag <72049372+magnuhag@users.noreply.github.com>
2022-07-02 21:11:33 +0200

    Update viaplay.py

--- web_scrape/viaplay.py
+++ web_scrape/viaplay.py
@@ -1,5 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import pandas as pd
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+#/usr/bin/safaridriver
+driver = webdriver.Safari()
+#link to viaplay player
+driver.get("https://viaplay.no/player/default/serier/californication/sesong-2/episode-11")
+
+#<button id="onetrust-pc-btn-handler" class="cookie-setting-link">Cookies Settings</button>
+button1 = driver.find_element(By.ID, "onetrust-pc-btn-handler")
+button1.click()
+
+#<button class="save-preference-btn-handler onetrust-close-btn-handler" tabindex="0">Confirm My Choices</button>
+button2 = driver.find_element(By.NAME, "save-preference-btn-handler onetrust-close-btn-handler")
+
+#<button class="skip-preliminaries-button">Hopp over</button>
