735fbe2ad02ed66ae04fc435aa98f742503553e4
magnuhag <72049372+magnuhag@users.noreply.github.com>
2023-04-14 11:59:31 +0200

    Sep

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -1,6 +1,7 @@
 import autograd.numpy as np
 from autograd import jacobian
 from autograd import elementwise_grad as egrad
+
 class NeuralNet:
 
     def __init__(self):
@@ -192,7 +193,7 @@ class NeuralNet:
             metric_val = np.mean(self.metrics(predicted, y_mini, metric))
             loss_val = np.mean(self.loss_function(loss)(predicted, y_mini))
             #Yes, formatting
-            print("mean loss = %.3f ---------- %s = %.2f at epoch %g" %(loss_val, metric, metric_val, i))
+
 
     def metrics(self, y_hat, y, a):
         """
@@ -223,68 +224,3 @@ class NeuralNet:
         """
         self.feed_forward(X)
         return self.A[-1]
-
-if __name__ == "__main__":
-    from sklearn import datasets
-    import pandas as pd
-    from sklearn.model_selection import train_test_split
-    from sklearn.preprocessing import StandardScaler
-    from time import perf_counter
-
-    # one-hot encoding the targest
-    def to_categorical_numpy(integer_vector):
-        n_inputs = len(integer_vector)
-        n_categories = np.max(integer_vector) + 1
-        onehot_vector = np.zeros((n_inputs, n_categories))
-        onehot_vector[range(n_inputs), integer_vector] = 1
-        return onehot_vector
-
-    def fix_data():
-        # download MNIST dataset
-        digits = datasets.load_digits()
-
-        # define inputs and labels
-        inputs = digits.images
-        labels = digits.target
-
-
-
-        n_inputs = len(inputs)
-        inputs = inputs.reshape(n_inputs, -1)
-        X = inputs
-        Y = to_categorical_numpy(labels)
-        return X, Y
-
-    X, Y = fix_data()
-
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2 )
-
-    in_size = len(X[0])
-    out_size = len(y_test[0])
-
-    Net = NeuralNet()
-    #We don't need anything fancy for this demonstration. 1 hidden layer is enough
-    Net.add(in_size, "sigmoid", input_size = in_size)
-    Net.add(10, "softmax")
-
-    t_train_start = perf_counter()
-
-    #We're leaving batch size at a modest 10 as to not having to spend all day training the network
-    Net.train(X_train, y_train, 100, "categorical_cross", "accuracy", batch_size = 10, num_iters = 100)
-
-    t_train_stop = perf_counter()
-
-    print(t_train_stop-t_train_start)
-
-    #Reminder of args and kwargs
-    #train(self, X, y, epochs, loss, metric, batch_size = 10, num_iters = 50, eta_init = 10**(-4), decay = 0.1)
-
-
-    pred = Net.predict(X_test)
-    s = 0
-    for i in range(len(X_test)):
-        true = np.argmax(y_test[i])
-        guess = np.argmax(pred[i])
-        if true == guess:
-            s += 1
-    print("test accuracy is " + str(s/len(y_test)))
